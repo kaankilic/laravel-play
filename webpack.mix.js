112395a5a7189ddfbbde9433e6392da0313aac20
Kaan Kilic <bl4cksta@gmail.com>
2019-09-18 01:46:32 +0300

    webpack path issue fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,8 @@ require('laravel-mix-merge-manifest');
 
 mix.setPublicPath('../../public').mergeManifest();
 
-mix.js(__dirname + '/Resources/assets/js/app.js', 'js/laravelplay.js')
-    .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/laravelplay.css');
+mix.js(__dirname + '/src/Resources/assets/js/app.js', 'js/laravelplay.js')
+    .sass( __dirname + '/src/Resources/assets/sass/app.scss', 'css/laravelplay.css');
 
 if (mix.inProduction()) {
     mix.version();
