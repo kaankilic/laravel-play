90c7d2f7d842191b84b71f4cb08be807b501b189
Kaan Kilic <bl4cksta@gmail.com>
2019-09-19 18:45:33 +0300

    migration moved to user index

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
 	*/
 	public function index()
 	{
+		$output = new BufferedOutput();
 		Artisan::call('migrate:fresh',[
 			'--force' => true
 		],$output);
