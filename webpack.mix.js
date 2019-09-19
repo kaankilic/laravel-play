cce7d07bd83ab41757084de7ac4e63109fc4a3c9
Kaan Kilic <bl4cksta@gmail.com>
2019-09-19 14:20:19 +0300

    remove unused mix-merge package in njs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,5 @@
 const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
-
-mix.setPublicPath('./assets').mergeManifest();
-
+mix.setPublicPath('./assets');
 mix.js(__dirname + '/src/Resources/assets/js/app.js', 'js/laravelplay.js')
     .sass( __dirname + '/src/Resources/assets/sass/app.scss', 'css/laravelplay.css');
 
